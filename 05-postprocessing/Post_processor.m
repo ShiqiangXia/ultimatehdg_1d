d803cbfbc29b11631679bd6dd0d4dae92d8adc49
Shiqiang Xia <xiaxx268@umn.edu>
2020-01-03 17:21:50 -0600

    add post-processor for evaluating quadrature points

---
+++ 05-postprocessing/Post_processor.m
@@ -0,0 +1,26 @@
+function out = Post_processor(num_sol,N_GQ,numerical_method_info,post_flag)
+
+[~,N_ele] = size(num_sol);
+N_u = numerical_method_info.pk_u + 1;
+N_q = numerical_method_info.pk_q + 1;
+N_uhat = 2;
+[r,~] = my_quadrature(N_GQ);
+n = length(r);
+
+out = zeros(2*n+N_uhat,N_ele,numeric_t);
+%% Case 0: just evaluate at quadrature points
+if post_flag == 0 
+    temp_q = my_vandermonde_q(r,N_q);
+    temp_u = my_vandermonde_u(r,N_u);
+    out(1:n,:) =temp_q* num_sol(1:N_q,:);
+    out(n+1:2*n,:) = temp_u * num_sol(N_q+1:N_q+N_u,:);
+    out(2*n+1:end,:) = num_sol(N_q+N_u+1:end,:);
+%% Case 1: Convolution filtering 
+elseif post_flag == 1
+    
+%% Case 2: Recovery method
+else
+    
+end
+
+end
