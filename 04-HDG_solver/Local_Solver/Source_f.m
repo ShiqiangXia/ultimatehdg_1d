a87bac7412a4c703af0eefdd164ecfee7c485f7d
Shiqiang Xia <xiaxx268@umn.edu>
2020-01-03 16:20:36 -0600

    add exact solution

--- 04-HDG_solver/Local_Solver/Source_f.m
+++ 04-HDG_solver/Local_Solver/Source_f.m
@@ -6,7 +6,7 @@ function my_f = Source_f(exact_func,xL,xR,N_GQ,N_u)
 h = abs(xR - xL);
 mid = abs(xR+xL)/numeric_t('2');
 
-f_values = exact_func(Ref_phy_map(r,h,mid),2);
+f_values = exact_func(Ref_phy_map(r,h,mid),2); % f = - Laplace u
 
 temp_basis = my_vandermonde_u(r,N_u);
 
--- 11-exact_functions/smooth_func101.m
+++ 11-exact_functions/smooth_func101.m
@@ -1,10 +1,10 @@
-function out = smooth_func(x,flag)
+function out = smooth_func101(x,flag)
 my_pi = numeric_t('pi');
 if flag == 0 % exact solution u
     out = sin(my_pi*x);
 elseif flag == 1 % exact q: -grad u
-    out = - cos(my_pi*x);
+    out = - my_pi*cos(my_pi*x);
 else             % div*q, namely - Laplace u
-    out = sin(my_pi*x);
+    out = my_pi*my_pi*sin(my_pi*x);
 end
 end
