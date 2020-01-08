f9acbcff87b75681a3bcbe7d3b183340b6ef751e
Shiqiang Xia <xiaxx268@umn.edu>
2020-01-08 17:18:39 -0600

    comment convolution matrix

--- 05-postprocessing/data/Convolution_matrix.m
+++ 05-postprocessing/data/Convolution_matrix.m
@@ -1,21 +1,33 @@
+% Oliver Xia 01/08/2020
+% Return Convolution matrix 
+% #points x Nu x (2k+1) x (4k+1)
+
 function AA = Convolution_matrix(k,Nu,pts,N_GQ)
 
 % use Bspine of order k+1;
-% Polynomial degree is Np-1;
+% Polynomial degree is Nu-1;
 % pts are the points of interest;
-% pts need to be a column vector
+% pts need to be a COLUMN vector
 
 deg_bspine = k+1;
 [r,w] = my_quadrature(N_GQ);
 
 n = length(pts);
 AA = zeros(n,Nu,2*k+1,4*k+1,numeric_t);
+
+% compute Gauss Quadratue point values of basis functions
 basis_mtrix = my_vandermonde_u(r,Nu);
 
 for rr = -k:1:k
     for jj = -2*k:1:2*k
+        % compute pt_n/2 - jj - r_n/2 -r
         temp = bsxfun(@minus,pts,r')./numeric_t("2") - jj - rr;
+        
+        % compute Bspine_k+1 (pt_n/2 - jj - r_n/2 -r)
+        % matrix: each row same point, diffent Gauss points rn
         bspine_matrix = Bspine(temp,deg_bspine);
+        
+        % numerical quadratue
         AA(:,:,rr+k+1,jj+2*k+1)=bspine_matrix * (w.*basis_mtrix);
     end
 end
