d9bb7aace3e1b79a96e7733a0507b931fc06cd08
Shiqiang Xia <xiaxx268@umn.edu>
2020-02-07 17:22:33 -0600

    idea <uh-uh_hat,ph-ph_hat> seems to be a very good error indicator, the next step is to find a good numerical example and refine strategy

--- 07-adaptivity/Functional_Adaptivity.m
+++ 07-adaptivity/Functional_Adaptivity.m
@@ -2,10 +2,13 @@ function refine_vector = Functional_Adaptivity(refine_number,error_estimator,tol
 [~,N_ele] = size(error_estimator);
 error_estimator = abs(error_estimator);
 refine_vector = zeros(1,N_ele,numeric_t);
+percent = 30;
 if refine_number == 2 % First Adaptive method 
     if adp_strategy == 1
         %e_tol = tol/N_ele;
         e_tol = sum(error_estimator)/N_ele;
+        %e_tol = prctile(error_estimator,percent);
+        
         idx = (error_estimator>e_tol);
         refine_vector(idx) = 1;
     end
